1f015423f81c34f046e54e1f164364355523d34e
DylanCharton <d.charton@codeur.online>
2021-09-20 22:48:20 +0200

    Mise en forme de la navbar

--- php/header.php
+++ php/header.php
@@ -1,15 +1,39 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Music Band - La musique au bout des doigts</title>
-    <meta name="description" content="Music Band vous permet d'écouter votre musique préférée où vous voulez. Profitez de podcasts exclusifs et de playlists sur-mesure construite à partir de plus de cinquante millions de titres.">
+    <meta name="description"
+        content="Music Band vous permet d'écouter votre musique préférée où vous voulez. Profitez de podcasts exclusifs et de playlists sur-mesure construite à partir de plus de cinquante millions de titres.">
     <!-- BOOTSTRAP -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <!-- CSS -->
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    
+    <header class="container-fluid">
+        <nav class="navbar-nav d-flex flex-row align-items-center">
+            <div class="menu-btn d-flex justify-content-center col-2">
+                <img src="img/menuburger.svg" alt="Menu Burger">
+            </div>
+            <div class="d-flex col-5 logo ps-3 align-items-center">
+                <img src="img/logo.svg" alt="logo du site">
+                <p class="ms-3 brand">Music Band <br>
+                <span class="sub-brand">La musique au bout des doigts</span></p>
+            </div>
+            <form action="" id="searchfield" class="d-flex">
+                <input type="search" size="35" placeholder="Recherchez, écoutez...">
+                <button type="submit"><img src="img/headphones.svg" alt=""></button>
+            </form>
+            <div id="account">
+                <img src="img/user.svg" alt="icone utilisateur">
+            </div>
+            <p class="m-0">Connexion / Inscription</p>
+        </nav>
+
+    </header>
