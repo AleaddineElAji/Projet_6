7f4819bae4514399ca9d02028e3d052493d6323d
DylanCharton <d.charton@codeur.online>
2021-09-20 16:21:08 +0200

    added the images of the header

--- index.php
+++ index.php
@@ -1,29 +1,30 @@
 <!-- Including the head and header -->
-<?php include "php/header.php" ?>
+<?php include ('php/header.php') 
+    ?>
 
 <!-- Including the genre selection part -->
 <section id="genre">
-    <?php include "php/genre.php" ?>
+    <?php include 'php/genre.php' ?>
 </section>
 <!-- Including the tracklist part -->
 <section id="tracklist">
-    <?php include "php/tracklist.php" ?>
+    <?php include 'php/tracklist.php' ?>
 </section>
-<!-- Including the "artist of the moment" part -->
+<!-- Including the 'artist of the moment' part -->
 <section id="suggest">
-    <?php include "php/suggest.php" ?>
+    <?php include 'php/suggest.php' ?>
 </section>
-<!-- Including the "best playlists" part -->
+<!-- Including the 'best playlists' part -->
 <section id="best-playlists">
-    <?php include "php/playlists" ?>
+    <?php include 'php/playlists' ?>
 </section>
 <!-- Including the radios part -->
 <section id="radios">
-    <?php include "php/radios.php" ?>
+    <?php include 'php/radios.php' ?>
 </section>
 <!-- Including the fixed-bottom audio player -->
-<?php include "php/audio-player" ?>
+<?php include 'php/audio-player' ?>
 
 <!-- Including the footer -->
-<?php include "php/footer.php" ?>
+<?php include 'php/footer.php' ?>
     
