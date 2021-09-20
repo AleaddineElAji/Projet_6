756eafa9955a5c80d9d38730adaed5a59fd2d7a4
DylanCharton <d.charton@codeur.online>
2021-09-20 15:46:52 +0200

    Structure of the page

---
+++ index.php
@@ -0,0 +1,29 @@
+<!-- Including the head and header -->
+<?php include "php/header.php" ?>
+
+<!-- Including the genre selection part -->
+<section id="genre">
+    <?php include "php/genre.php" ?>
+</section>
+<!-- Including the tracklist part -->
+<section id="tracklist">
+    <?php include "php/tracklist.php" ?>
+</section>
+<!-- Including the "artist of the moment" part -->
+<section id="suggest">
+    <?php include "php/suggest.php" ?>
+</section>
+<!-- Including the "best playlists" part -->
+<section id="best-playlists">
+    <?php include "php/playlists" ?>
+</section>
+<!-- Including the radios part -->
+<section id="radios">
+    <?php include "php/radios.php" ?>
+</section>
+<!-- Including the fixed-bottom audio player -->
+<?php include "php/audio-player" ?>
+
+<!-- Including the footer -->
+<?php include "php/footer.php" ?>
+    
