d592991eb780ed295620b2b26ff4279181c6504a
FReaper39 <a.elaji@codeur.online>
2021-09-27 14:31:39 +0200

    alaedinwindow

--- php/footer.php
+++ php/footer.php
@@ -6,15 +6,15 @@
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     <script>   
         if(window.innerWidth>1400) {
-            var swiper = new Swiper(".mySwiper", {slidesPerView: 9.25, spaceBetween: 30, freeMode: true,});
+            var swiperA = new Swiper(".mySwiperA", {slidesPerView: 9.25, spaceBetween: 30, freeMode: true,});
             }else if(window.innerWidth>1200){
-                var swiper = new Swiper(".mySwiper", {slidesPerView: 6.75, spaceBetween: 30, freeMode: true,});
+                var swiperA = new Swiper(".mySwiperA", {slidesPerView: 6.75, spaceBetween: 30, freeMode: true,});
             }else if(window.innerWidth>1000){
-                var swiper = new Swiper(".mySwiper", {slidesPerView: 4.75, spaceBetween: 30, freeMode: true,});
-            }else if(window.innerWidth>680){
-                var swiper = new Swiper(".mySwiper", {slidesPerView: 3.75, spaceBetween: 30, freeMode: true,});
+                var swiperA = new Swiper(".mySwiperA", {slidesPerView: 4.75, spaceBetween: 30, freeMode: true,});
+            }else if(window.innerWidth>630){
+                var swiperA = new Swiper(".mySwiperA", {slidesPerView: 3, spaceBetween: 30, freeMode: true,});
             }else if(window.innerWidth>350){
-                var swiper = new Swiper(".mySwiper", {slidesPerView: 1.75, spaceBetween: 30, freeMode: true,});
+                var swiperA = new Swiper(".mySwiperA", {slidesPerView: 1.75, spaceBetween: 30, freeMode: true,});
             }
   </script>
 </html>
